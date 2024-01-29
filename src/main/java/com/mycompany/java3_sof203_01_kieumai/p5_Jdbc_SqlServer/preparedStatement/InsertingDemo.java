02c53300342a2eab3a5709a276a7dd94b961084c
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2024-01-29 19:33:36 +0700

    Prepared Statement: Creating table and Inserting data

--- src/main/java/com/mycompany/java3_sof203_01_kieumai/p5_Jdbc_SqlServer/preparedStatement/InsertingDemo.java
+++ src/main/java/com/mycompany/java3_sof203_01_kieumai/p5_Jdbc_SqlServer/preparedStatement/InsertingDemo.java
@@ -2,13 +2,13 @@ package com.mycompany.java3_sof203_01_kieumai.p5_Jdbc_SqlServer.preparedStatemen
 
 import com.mycompany.java3_sof203_01_kieumai.p5_Jdbc_SqlServer.DatabaseConnectionManager_SQLServer;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.PreparedStatement;
 
-public class CreateTableDemo {
+public class InsertingDemo {
 
-    public static void main(String[] args) throws SQLException {
+        public static void main(String[] args) throws SQLException {
         DatabaseConnectionManager_SQLServer dcm
                 = new DatabaseConnectionManager_SQLServer("test", "sa", "123456");
 
@@ -24,17 +24,18 @@ public class CreateTableDemo {
             // B1: sql
             String sql
                     = """
-                        CREATE TABLE employee2(
-                           Id INT NOT NULL,
-                           Name NVARCHAR(50) NOT NULL,
-                           Salary DECIMAL NOT NULL,
-                           PRIMARY KEY (Id)
-                        );
+                        INSERT INTO employee2(Id, Name, Salary)
+                        VALUES(?, ?, ?);
                     """;
             // B2: run
             preparedStatement = connection.prepareStatement(sql);
 
-            preparedStatement.execute();
+            // set values for ?, ?, ?
+            preparedStatement.setInt(1, 1001);
+            preparedStatement.setString(2, "Mike");
+            preparedStatement.setDouble(3, 50000);
+
+            preparedStatement.executeUpdate();
 
             System.out.println("done...");
 
@@ -52,5 +53,4 @@ public class CreateTableDemo {
             }
         }
     }
-
 }
